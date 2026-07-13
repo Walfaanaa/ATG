d7e63b9bfc4c9c0d221f3a00809df3d49f5bd834
Walfaanaa Magarsaa Ejeta <119677853+Walfaanaa@users.noreply.github.com>
2026-07-13 13:40:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ EXCEL_URL = "https://raw.githubusercontent.com/Walfaanaa/ATG/main/ATG.xlsx"
 
 df = pd.read_excel(EXCEL_URL, engine="openpyxl")
 
-st.title("EGSA Monthly Payment System")
+st.title("Sirna To`annaa Buusii Ji`aa fi Dabalataa Afoosha Tokkummaa Gaalessaa")
 st.dataframe(df)

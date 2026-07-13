9d360a6b0f6a094acb6a3d9a0c1208813c409dd3
Walfaanaa Magarsaa Ejeta <119677853+Walfaanaa@users.noreply.github.com>
2026-07-13 13:44:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
 import streamlit as st
 import pandas as pd
 
-EXCEL_URL = "https://raw.githubusercontent.com/Walfaanaa/ATG/main/ATG.xlsx"
+MONTHLY_PAYMENT = 200
+PENALTY = MONTHLY_PAYMENT * 0.5
 
-df = pd.read_excel(EXCEL_URL, engine="openpyxl")
+df = pd.read_excel("ATG.xlsx")
 
-st.title("Sirna To`annaa Buusii Jihaa fi Dabalataa Afoosha Tokkummaa Gaalessaa")
-st.dataframe(df)
+df["buusii_jiaa"] = df["buusii_jiaa"].fillna(0)
+
+# KPIs
+total_members = len(df)
+
+paid_members = (df["buusii_jiaa"] >= MONTHLY_PAYMENT).sum()
+
+unpaid_members = (df["buusii_jiaa"] < MONTHLY_PAYMENT).sum()
+
+total_collected = df["buusii_jiaa"].sum()
+
+total_penalty = unpaid_members * PENALTY
+
+# Add penalty column
+df["Penalty"] = df["buusii_jiaa"].apply(
+    lambda x: PENALTY if x < MONTHLY_PAYMENT else 0
+)
+
+# Members who didn't pay
+non_paid = df[df["buusii_jiaa"] < MONTHLY_PAYMENT]

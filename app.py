80b16375f2a86300e1706b19f95668b6474d6690
Walfaanaa Magarsaa Ejeta <119677853+Walfaanaa@users.noreply.github.com>
2026-07-16 09:40:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -338,7 +338,7 @@ chart_df = pd.DataFrame({
         "Monthly Payment",
         "Additional Payment",
         "Total Capital of AO",
-        "Total Cost Of AO",
+        "Total Incrued Cost Of AO",
         "Currect Balance Of AO"
     ],
 
